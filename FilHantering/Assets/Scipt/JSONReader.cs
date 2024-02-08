e1d8d30ed967d145002ac61004464c0197d908f7
Bard1en <fowzino16@gmail.com>
2024-02-08 14:05:28 +0100

    save&Load

---
+++ FilHantering/Assets/Scipt/JSONReader.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JSONReader : MonoBehaviour
+{
+public TextAsset textJSON;
+
+[System.Serializable]
+public class Player{
+    public string name;
+    public int health;
+    public int mana;
+}
+
+[System.Serializable]
+
+public class PlayerList 
+{
+    public Player []player;
+}
+
+public PlayerList myplayerlist = new PlayerList();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myplayerlist = JsonUtility.FromJson<PlayerList>(textJSON.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
